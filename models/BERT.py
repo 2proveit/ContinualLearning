3c3402a966a18909b246fbc790f0bc2e5654c479
Xinlong Li <lxl2067956020@gmail.com>
2023-06-08 21:36:34 +0800

    modified some model file

--- models/BERT.py
+++ models/BERT.py
@@ -1,9 +1,6 @@
-import transformers
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss, MSELoss
-from transformers import BertTokenizer, BertModel, BertConfig
+from transformers import BertModel, BertConfig
 
 
 class BertClass(nn.Module):
@@ -16,15 +13,6 @@ class BertClass(nn.Module):
         self.pre_classifier = nn.Linear(config.hidden_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, 2)
-        # implement some fine-tune methods
-        if args.ft_method == 'prefix':
-            modules = [self.bert.embeddings, self.bert.encoder.layer[:args.activate_layer_num]]
-            modules = [self.bert.encoder.layer[-1]]
-
-            for module in modules:
-                for param in module.parameters():
-                    param.requires_grad = False
-            print('bert encoder layer fixed!')
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         bert_out = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
